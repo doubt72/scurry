34122043aa63dfbee8e48e817ecabd9447d5c017
doubt72 <douglas@triggs.org>
2017-04-20 18:10:38 -0600

    Initial Commit

---
+++ scurry/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  scurry
+//
+//  Created by Douglas Triggs on 4/20/17.
+//  Copyright © 2017 Douglas Triggs. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
